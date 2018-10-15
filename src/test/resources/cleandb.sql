958c5e52e0b60dd4383bb000d1f8db66224e4060
student <kump@madisoncollege.edugit config --global user.email kump@madisoncollege.edu>
2018-10-15 06:09:43 -0500

    Added cleandb.sql.

---
+++ src/test/resources/cleandb.sql
@@ -0,0 +1,14 @@
+delete from user;
+delete from userSearchResult;
+INSERT INTO `user` VALUES (1,'Mark','Sierra'),(2,'Steve','Schilt'),(3,'Bill','Ottinger');
+INSERT INTO `userSearchResult` VALUES (1,'Head First Java, 2nd Edition',1,'978-0596009205',2005),(2,'Beginning Hibernate',3,'978-1-4842-2319-2',2016),(3,'Java: A Beginner’s Guide (Sixth Edition)',2,'978-0071809252',2014),(14,'My Test Book',NULL,'test isbn',2000);
+
+fname
+
+lname
+
+email
+
+userName
+
+pword
