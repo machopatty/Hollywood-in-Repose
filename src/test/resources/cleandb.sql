91e6e15f9b0ef55cf7d26f47fbc5ba1a107a65f3
student <kump@madisoncollege.edugit config --global user.email kump@madisoncollege.edu>
2018-10-17 16:54:45 -0500

    reversed delete table order.

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,5 +1,7 @@
-delete from user;
 delete from userSearchResult;
+delete from user;
 INSERT INTO `user` VALUES (1,'Mark','Sierra','mSierra','xxxxx','sierra@hotmail.com'),(2,'Steve','Francis','sFrancis','xxxxx','sFrancis@hotmail.com'),(3,'Bill','Jenkins','bJenkins','xxxxx','bJenkins@hotmail.com');
 INSERT INTO `userSearchResult` VALUES (1,'John Candy',1),(2,'Slim Pickens',3),(3,'David Bowie',2);
+
+
 
