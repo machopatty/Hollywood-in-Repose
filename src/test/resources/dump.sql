88ac7b53aa324ee0144bb964d68d3d5d22e50d71
student <kump@madisoncollege.edugit config --global user.email kump@madisoncollege.edu>
2018-10-17 19:15:29 -0500

    fixed sql dump.

--- src/test/resources/dump.sql
+++ src/test/resources/dump.sql
@@ -16,13 +16,13 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `author`
+-- Table structure for table `user`
 --
 
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `author` (
+CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(25) NOT NULL,
   `last_name` varchar(30) NOT NULL,
@@ -30,16 +30,16 @@ CREATE TABLE `author` (
   `password` varchar(30) NOT NULL,
   `email` varchar(30) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `author_id_uindex` (`id`)
+  UNIQUE KEY `user_id_uindex` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `author`
+-- Dumping data for table `user`
 --
 
 LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `author` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` VALUES (1,'Mark','Sierra','mSierra','xxxxx','sierra@hotmail.com'),(2,'Steve','Francis','sFrancis','xxxxx','sFrancis@hotmail.com'),(3,'Bill','Jenkins','bJenkins','xxxxx','bJenkins@hotmail.com');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;users_user_name_uindex
@@ -57,7 +57,7 @@ CREATE TABLE `userSearchResult` (
   `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `userSearchResult_id_uindex` (`id`),
-  KEY `book_user_id_fk` (`user_id`),
+  KEY `userSearchResult_user_id_fk` (`user_id`),
   CONSTRAINT `userSearchResult_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
